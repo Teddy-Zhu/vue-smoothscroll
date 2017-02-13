e44bc36433a60c4e9100259ffea3f2c2d2cb6939
teddyzhu <teddyzhu15@gmail.com>
2017-02-13 14:00:56 +0800

    update smoothscroll version to 0.3.0

--- dist/vue-smoothscroll.js
+++ dist/vue-smoothscroll.js
@@ -104,10 +104,10 @@ return /******/ (function(modules) { // webpackBootstrap
 	if(document.querySelectorAll === void 0 || window.pageYOffset === void 0 || history.pushState === void 0) { return; }
 
 	// Get the top position of an element in the document
-	var getTop = function(element) {
+	var getTop = function(element, start) {
 	    // return value of html.getBoundingClientRect().top ... IE : 0, other browsers : -pageYOffset
-	    if(element.nodeName === 'HTML') return -window.pageYOffset
-	    return element.getBoundingClientRect().top + window.pageYOffset;
+	    if(element.nodeName === 'HTML') return -start
+	    return element.getBoundingClientRect().top + start
 	}
 	// ease in out function thanks to:
 	// http://blog.greweb.fr/2012/02/bezier-curve-based-easing-functions-from-concept-to-implementation/
@@ -127,16 +127,16 @@ return /******/ (function(modules) { // webpackBootstrap
 	// if the first argument is an element then scroll to the top of this element
 	// if the first argument is numeric then scroll to this location
 	// if the callback exist, it is called when the scrolling is finished
-	// if context is set then scroll that element, else scroll window 
+	// if context is set then scroll that element, else scroll window
 	var smoothScroll = function(el, duration, callback, context){
 	    duration = duration || 500;
 	    context = context || window;
-	    var start = window.pageYOffset;
+	    var start = context.scrollTop || window.pageYOffset;
 
 	    if (typeof el === 'number') {
 	      var end = parseInt(el);
 	    } else {
-	      var end = getTop(el);
+	      var end = getTop(el, start);
 	    }
 
 	    var clock = Date.now();
@@ -147,10 +147,10 @@ return /******/ (function(modules) { // webpackBootstrap
 	    var step = function(){
 	        var elapsed = Date.now() - clock;
 	        if (context !== window) {
-	        	context.scrollTop = position(start, end, elapsed, duration);
+	          context.scrollTop = position(start, end, elapsed, duration);
 	        }
 	        else {
-	        	window.scroll(0, position(start, end, elapsed, duration));
+	          window.scroll(0, position(start, end, elapsed, duration));
 	        }
 
 	        if (elapsed > duration) {
@@ -172,7 +172,10 @@ return /******/ (function(modules) { // webpackBootstrap
 	    // most browser don't update :target when the history api is used:
 	    // THIS IS A BUG FROM THE BROWSERS.
 	    // change the scrolling duration in this call
-	    smoothScroll(document.getElementById(this.hash.substring(1)), 500, function(el) {
+	    var node = document.getElementById(this.hash.substring(1))
+	    if(!node) return; // Do not scroll to non-existing node
+
+	    smoothScroll(node, 500, function(el) {
 	        location.replace('#' + el.id)
 	        // this will cause the :target to be activated.
 	    });
